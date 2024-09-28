a23c7f5a2a0105151d0834bd0e751e9a705a01d1
Maanola08 <maanpajimola@gmail.com>
2024-09-28 15:10:42 +0800

    message

--- client/src/views/orderstatus/OrderStatus.js
+++ client/src/views/orderstatus/OrderStatus.js
@@ -16,6 +16,21 @@ CDropdownMenu,
 CDropdownDivider
 } from '@coreui/react';
 
+const btn = () =>{
+        return(
+
+          <div className="d-grid gap-2 d-md-flex justify-content-md-end">
+          <CButton color="primary" className="me-md-2">Button</CButton>
+          <CButton color="primary">Button</CButton>
+        </div>
+
+
+
+        )
+}
+
+export default btn; 
+
 const orderstatus = () =>{
     
     return (
@@ -30,6 +45,7 @@ const orderstatus = () =>{
       <CTableHeaderCell scope="col">Email</CTableHeaderCell>
       <CTableHeaderCell scope="col">BillingAddress</CTableHeaderCell>
       <CTableHeaderCell scope="col">Contact</CTableHeaderCell>
+      <CTableHeaderCell scope="col">RecipientAddress</CTableHeaderCell>
       <CTableHeaderCell scope="col">Name</CTableHeaderCell>
       <CTableHeaderCell scope="col">Contact</CTableHeaderCell>
       <CTableHeaderCell scope="col">Kg</CTableHeaderCell>
@@ -40,32 +56,17 @@ const orderstatus = () =>{
     <CTableRow>
        <CTableDataCell><CFormCheck id="flexCheckDefault"/></CTableDataCell> 
       <CTableHeaderCell scope="row">1</CTableHeaderCell>
-      <CTableDataCell>
-      
-        <CDropdown variant="btn-group">
-        <CDropdownToggle color="secondary" size="sm">Status</CDropdownToggle>
-        <CDropdownMenu>
-        <CDropdownItem href="#">Processing</CDropdownItem>
-        <CDropdownItem href="#">Failed</CDropdownItem>
-        <CDropdownItem href="#">SuccessShippiment</CDropdownItem>
-        <CDropdownDivider/>
-        </CDropdownMenu>
-        </CDropdown>
-
-      </CTableDataCell>
+      <CTableDataCell></CTableDataCell>
       <CTableDataCell>Ma.Angelica Olavidez</CTableDataCell>
       <CTableDataCell>maanpajimola@gmail.com</CTableDataCell>
       <CTableDataCell>2455 Pacamara st,Brgy Commonwealth QuezonCity</CTableDataCell>
       <CTableDataCell>09518941731</CTableDataCell>
+      <CTableDataCell>litex Commonwealth</CTableDataCell>
       <CTableDataCell>Rona irader</CTableDataCell>
       <CTableDataCell>0915648489</CTableDataCell>
       <CTableDataCell>5</CTableDataCell>
       <CTableDataCell>199</CTableDataCell>
-      <div className="d-grid gap-2 d-md-flex justify-content-md-end">
-    <CButtonGroup className="me-md-2">
-    <CButton as="a" color="primary" href="#" role="button">Submit</CButton>
-    </CButtonGroup>
-    </div>
+      
       </CTableRow>
       
   </CTableBody>
