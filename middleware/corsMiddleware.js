33d833a5f81b77b6533ecaa9479aed92748afc36
freight-capstone <nazzchwan@gmail.com>
2024-11-26 04:29:57 +0000

    Production

--- middleware/corsMiddleware.js
+++ middleware/corsMiddleware.js
@@ -1,7 +1,13 @@
 import cors from "cors";
 
 const corsMiddleware = cors({
-    origin: process.env.CLIENT_ORIGIN,
+    origin: (origin, callback) => {
+        if (["https://log2.axleshift.com", "http://localhost:3000"].includes(origin)) {
+            callback(null, true);
+        } else {
+            callback(new Error("Not allowed by CORS"));
+        }
+    },
     credentials: true,
 });
 
