14b36c039dfb038ceb3bc8d6491578b7bd8cc80f
Akuito <nazzchwan@gmail.com>
2024-11-28 03:37:05 +0800

    changes

--- client/src/components/supplierpage/SupplierFooter.js
+++ client/src/components/supplierpage/SupplierFooter.js
@@ -2,14 +2,16 @@ import React from 'react'
 import { CFooter } from '@coreui/react'
 
 const SupplierFooter = () => {
+  const currentYear = new Date().getFullYear()
+
   return (
     <CFooter className="px-4">
       <div>
         <a href="">CAPSTONE</a>
-        <span className="ms-1"></span>
+        <span className="ms-1">© {currentYear} All rights reserved</span> {/* Copyright text */}
       </div>
       <div className="ms-auto">
-        <span className="me-1"> Cluster L: </span>
+        <span className="me-1">Cluster L:</span>
         <a href="">Logistics 2: Inventory Management</a>
       </div>
     </CFooter>
